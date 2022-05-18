bdb07c7b71b798c86095382eda096bbfa964b1f4
leyendanerd <leyendanerd@gmail.com>
2022-05-18 16:41:33 -0400

    ultimate

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 from app import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello Wor!'
+    assert response.data == b'Hello World!'
     assert response.status_code == 200
 
 
